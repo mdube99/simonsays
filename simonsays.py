0554131d172beb9073d2ee1c2dcdfe2c735892a4
Mark <mjdube99@gmail.com>
2018-07-24 13:55:03 -0500

    first version of simon says

--- simonsays.py
+++ simonsays.py
@@ -1,19 +1,29 @@
 import time
 import random
+import RPi.GPIO as GPIO
+import LEDRGB as LED
+
+R_pin = 11
+G_pin = 12
+B_pin = 13
+LED.setup(R_pin, G_pin, B_pin)
 
 colors = ['R', 'G', 'B', 'Y']
 # this script appends a value to a list
 
-def append_list():
-	n = random.randint(0,3)
-	colors.append(colors[n])
-	color_string = ''.join(colors)
-	for i in range(0, len(colors)):
-		print colors[i].lower()
-		time.sleep(1)
+trand = random.randint(1,3) - .5
+
+def loop():
+	while True:
+		crand = random.randint(0,3)
+		LED.setColor(colors[crand])
+		time.sleep(trand)
+		LED.noColor()
 
 if __name__ == '__main__':
 	try:
-		append_list()
+		loop()
 	except KeyboardInterrupt:
-		print 'Good bye'
+		LED.destroy()
+		print "\n LED Turned off."
+
