caba66acc0a3390b99600975a55f5ab35ca8b80b
Mark <mjdube99@gmail.com>
2018-07-24 10:40:56 -0500

    print_list

---
+++ scripts/print_list.py
@@ -0,0 +1,16 @@
+import time
+
+#This script demonstrates looping through a list
+
+colors = ['R', 'G', 'B', 'Y']
+
+def print_list():
+	for i in range(0, len(colors)):
+		print colors[i]
+		time.sleep(1)
+
+if __name__ == '__main__':
+	try:
+		print_list()
+	except KeyboardInterrupt:
+		print 'Good bye'
